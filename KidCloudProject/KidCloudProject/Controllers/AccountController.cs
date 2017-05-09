402d04f56b2d17822f3b4c58e5d847505c9681c2
Evan-Farr <evan.c.farr@gmail.com>
2017-05-09 16:44:29 -0500

    set up UserName and UserRoles

--- KidCloudProject/KidCloudProject/Controllers/AccountController.cs
+++ KidCloudProject/KidCloudProject/Controllers/AccountController.cs
@@ -17,6 +17,7 @@ namespace KidCloudProject.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        public ApplicationDbContext context;
 
         public AccountController()
         {
@@ -75,11 +76,12 @@ namespace KidCloudProject.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    if (User.IsInRole("Admin")) { return RedirectToLocal(returnUrl); }
+                    else { return RedirectToAction("Index", "Users"); }
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -139,6 +141,7 @@ namespace KidCloudProject.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
+            ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin")).ToList(), "Name", "Name");
             return View();
         }
 
@@ -151,20 +154,32 @@ namespace KidCloudProject.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.UserName, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                    return RedirectToAction("Index", "Home");
+                    await this.UserManager.AddToRoleAsync(user.Id, model.UserRoles);
+                    if (model.UserRoles == "Parent")
+                    {
+                        return RedirectToAction("Create", "Parents");
+                    }
+                    else if (model.UserRoles == "Employee")
+                    {
+                        return RedirectToAction("PrivateCreate", "Employees");
+                    }
+                    else if (model.UserRoles == "DayCare")
+                    {
+                        return RedirectToAction("PrivateCreate", "DayCares");
+                    }
                 }
+                ViewBag.Name = new SelectList(context.Roles.Where(u => !u.Name.Contains("Admin")).ToList(), "Name", "Name");
                 AddErrors(result);
             }
 
