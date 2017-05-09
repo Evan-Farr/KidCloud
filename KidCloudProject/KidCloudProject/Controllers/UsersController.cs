4e64dfa7a29dc0a51383aa9bca301ab053122d72
Evan-Farr <evan.c.farr@gmail.com>
2017-05-09 16:28:24 -0500

    setup user roles in SetUp, added usersController

---
+++ KidCloudProject/KidCloudProject/Controllers/UsersController.cs
@@ -0,0 +1,89 @@
+﻿using KidCloudProject.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashPickup.Controllers
+{
+    [Authorize]
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ViewBag.Name = user.Name;
+                ViewBag.displayMenu = "No";
+                ApplicationDbContext db = new ApplicationDbContext();
+                if (isUser("Admin"))
+                {
+                    ViewBag.displayMenu = "Yes";
+                }
+                else if (isUser("Parent"))
+                {
+                    string holder = user.GetUserId();
+                    var temp = db.Parents.Where(i => i.UserId.Id == holder).FirstOrDefault().Id;
+                    ViewBag.Id = temp;
+                    ViewBag.displayMenu = "Parent";
+                }
+                else if (isUser("Child"))
+                {
+                    string holder = user.GetUserId();
+                    var temp = db.Children.Where(i => i.UserId.Id == holder).FirstOrDefault().Id;
+                    ViewBag.Id = temp;
+                    ViewBag.displayMenu = "Child";
+                }
+                else if (isUser("Employee"))
+                {
+                    string holder = user.GetUserId();
+                    var temp = db.Employees.Where(i => i.UserId.Id == holder).FirstOrDefault().Id;
+                    ViewBag.Id = temp;
+                    var temp1 = db.Employees.Where(i => i.UserId.Id == holder).FirstOrDefault().ZipCode;
+                    ViewBag.ZipCode = temp1;
+                    ViewBag.displayMenu = "Employee";
+                }
+                else if (isUser("DayCare"))
+                {
+                    string holder = user.GetUserId();
+                    var temp = db.DayCares.Where(i => i.UserId.Id == holder).FirstOrDefault().Id;
+                    ViewBag.Id = temp;
+                    var temp1 = db.DayCares.Where(i => i.UserId.Id == holder).FirstOrDefault().ZipCode;
+                    ViewBag.ZipCode = temp1;
+                    ViewBag.displayMenu = "DayCare";
+                }
+                return View();
+            }
+            else
+            {
+                ViewBag.Name = "Not Logged In";
+            }
+            return View();
+        }
+
+        public bool isUser(string role)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == role)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
--- KidCloudProject/KidCloudProject/Startup.cs
+++ KidCloudProject/KidCloudProject/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
 using Owin;
+using System;
+using System.Linq;
 
 [assembly: OwinStartupAttribute(typeof(KidCloudProject.Startup))]
 namespace KidCloudProject
@@ -9,6 +13,59 @@ namespace KidCloudProject
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            CreateRolesAndUsers();
+        }
+
+        private void CreateRolesAndUsers()
+        {
+            Models.ApplicationDbContext context = new Models.ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<Models.ApplicationUser>(new UserStore<Models.ApplicationUser>(context));
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+                var user = new Models.ApplicationUser();
+                user.UserName = "Admin";
+                user.Email = "admin@gmail.com";
+                string userPassword = "Admin123!";
+                var checkUser = userManager.Create(user, userPassword);
+                if (checkUser.Succeeded)
+                {
+                    var result = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Parent"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Parent";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("DayCare"))
+            {
+                var role = new IdentityRole();
+                role.Name = "DayCare";
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists("Child"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Child";
+                roleManager.Create(role);
+            }
         }
     }
 }
