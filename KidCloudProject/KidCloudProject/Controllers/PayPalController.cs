b6d6799f29d3b7eb16401a8f293bc69b9436b79f
lpopodi <lisa@lisapopodi.com>
2017-05-12 16:39:02 -0500

    balance reset after paypal success

--- KidCloudProject/KidCloudProject/Controllers/PayPalController.cs
+++ KidCloudProject/KidCloudProject/Controllers/PayPalController.cs
@@ -30,26 +30,12 @@ namespace KidCloudProject.Controllers
 
                 if (string.IsNullOrEmpty(payerId))
                 {
-                    //this section will be executed first because PayerID doesn't exist
-                    //it is returned by the create function call of the payment class
-
-                    // Creating a payment
-                    // baseURL is the url on which paypal sendsback the data.
-                    // So we have provided URL of this controller only
                     string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority +
                                 "/Paypal/PaymentWithPayPal?";
 
-                    //guid we are generating for storing the paymentID received in session
-                    //after calling the create function and it is used in the payment execution
-
                     var guid = Convert.ToString((new Random()).Next(100000));
 
-                    //CreatePayment function gives us the payment approval url
-                    //on which payer is redirected for paypal account payment
-
                     var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
-
-                    //get links returned from paypal in response to Create function call
 
                     var links = createdPayment.links.GetEnumerator();
 
@@ -73,11 +59,6 @@ namespace KidCloudProject.Controllers
                 }
                 else
                 {
-                    // This section is executed when we have received all the payments parameters
-
-                    // from the previous call to the function Create
-
-                    // Executing a payment
 
                     var guid = Request.Params["guid"];
 
@@ -93,7 +74,7 @@ namespace KidCloudProject.Controllers
             {
                 return View("FailureView");
             }
-
+            ResetBalanceFromSuccess();
             return View("SuccessView");
         }
 
@@ -112,41 +93,19 @@ namespace KidCloudProject.Controllers
             var customer = db.Parents.Where(p => p.UserId.Id == user).First();
             DateTime date = DateTime.Now;
             string todaysDate = date.ToLongDateString();
-            //similar to credit card create itemlist and add item objects to it
-            //var itemList = new ItemList() { items = new List<Item>() };
-
-            //itemList.items.Add(new Item()
-            //{
-            //    name = "DayCare Account Balance",
-            //    currency = "USD",
-            //    price = "25",
-            //    quantity = "1",
-            //    //sku = "sku"
-            //});
-
+            
             var payer = new Payer() { payment_method = "paypal" };
 
-            // Configure Redirect Urls here with RedirectUrls object
             var redirUrls = new RedirectUrls()
             {
                 cancel_url = redirectUrl,
                 return_url = redirectUrl
             };
 
-            // similar as we did for credit card, do here and create details object
-            //var details = new Details()
-            //{
-            //    tax = "1",
-            //    shipping = "1",
-            //    subtotal = "5"
-            //};
-
-            // similar as we did for credit card, do here and create amount object
             var amount = new Amount()
             {
                 currency = "USD",
-                total = customer.MoneyOwed.ToString(), // Total must be equal to sum of shipping, tax and subtotal.
-                //details = details
+                total = customer.MoneyOwed.ToString(),
             };
 
             var transactionList = new List<Transaction>();
@@ -156,7 +115,6 @@ namespace KidCloudProject.Controllers
                 description = customer.FirstName + " " + customer.LastName + " payment for child care services. " + todaysDate + "",
                 invoice_number = customer.Id + "-INV" + Guid.NewGuid().ToString(),
                 amount = amount,
-                //item_list = itemList
             });
 
             this.payment = new Payment()
@@ -170,6 +128,14 @@ namespace KidCloudProject.Controllers
             // Create a payment using a APIContext
             return this.payment.Create(apiContext);
 
+        }
+
+        public void ResetBalanceFromSuccess()
+        {
+            var user = User.Identity.GetUserId();
+            Parent parent = db.Parents.Where(a => a.UserId.Id == user).FirstOrDefault();
+            parent.MoneyOwed = 0M;
+            db.SaveChanges();
         }
 
     }
