4a8f9d0fd78951fe5074cdc0080504097bad921b
Travis <t.j.palmer@comcast.net>
2017-05-15 17:02:51 -0500

    Added unit test project. Created unit test files for some of the controllers and wrote some basic unit test

---
+++ KidCloudProject/KidCloudUnitTest/Controllers/ApplicationsUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KidCloudProject.Controllers;
+using System.Web.Mvc;
+
+
+
+namespace KidCloudUnitTest.Controllers
+{
+    [TestClass]
+    public class ApplicationsUnitTests
+    {
+        [TestMethod]
+        public void Controller_Applications_Index_Get()
+        {
+            // Arrange
+            ApplicationsController controller = new ApplicationsController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Controller_Applications_Details_Get()
+        {
+            // Arrange
+            ApplicationsController controller = new ApplicationsController();
+            int id = 2;
+
+            // Act
+            ViewResult result = controller.Details(id) as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ KidCloudProject/KidCloudUnitTest/Controllers/DayCaresUnitTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KidCloudProject.Controllers;
+using System.Web.Mvc;
+
+namespace KidCloudUnitTest.Controllers
+{
+    [TestClass]
+    public class DayCaresUnitTests
+    {
+        [TestMethod]
+        public void Controller_DayCares_Index_Get()
+        {
+            // Arrange
+            DayCaresController controller = new DayCaresController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+        [TestMethod]
+        public void Controller_DayCares_IndexParents_Get()
+        {
+            // Arrange
+            DayCaresController controller = new DayCaresController();
+            int id = 1;
+
+            // Act
+            ViewResult result = controller.IndexParents(id) as ViewResult;
+            var test = result.Model;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
