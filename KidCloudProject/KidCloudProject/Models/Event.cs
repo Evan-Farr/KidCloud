ad6dfb7da58e8609f21caa9d442c01aa90173014
chewie4304 <chewie4304@gmail.com>
2017-05-15 10:34:43 -0500

    added Event class with properties

---
+++ KidCloudProject/KidCloudProject/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KidCloudProject.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+
+        [Required]
+        public string title { get; set; }
+
+        public Boolean allDay { get; set; }
+
+        public string start { get; set; }
+
+        public string end { get; set; }
+
+        public Boolean overlap { get; set; }
+
+    }
+}
